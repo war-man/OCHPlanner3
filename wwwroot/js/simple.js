fff443470366f0408a36aff6a8c12f1e6a4f09bb
Eric Dugal <eric.dugal@hotmail.com>
2020-10-03 21:35:29 -0400

    Sync between field

--- wwwroot/js/simple.js
+++ wwwroot/js/simple.js
@@ -4,4 +4,19 @@
         findDefaultPrinter(true);
         printCommand();
     });
+
+    //replication for oil list value
+    $(document).on("change", $('select[name="oillist"]'), function () {
+        $('select[name="oillist-preview"]').val($('select[name="oillist"]').val());
+    });
+
+    //replication for comment
+    $(document).on("keyup", $("#comment"), function () {
+        $('input[name="comment-preview"]').val($('input[name="comment"]').val());
+    });
+
+    //replication for unitvalue
+    $(document).on("keyup", $("#unitvalue"), function () {
+        $('input[name="unitvalue-preview"]').val($('input[name="unitvalue"]').val());
+    });
 });
