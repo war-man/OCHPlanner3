56f94c7dc4f27305f4a22cb0f9cc2943fe6f9392
Eric Dugal <eric.dugal@hotmail.com>
2020-11-26 21:41:35 -0500

    Fix to SignMessage

--- Controllers/SignMessageController.cs
+++ Controllers/SignMessageController.cs
@@ -1,58 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OCHPlanner3.Helper;
+using OCHPlanner3.Services.Interfaces;
+using System;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 
 namespace OCHPlanner3.Controllers
 {
-    public class SignMessageController : Controller
+    [AllowAnonymous]
+    [MiddlewareFilter(typeof(LocalizationPipeline))]
+    public class SignMessageController : BaseController
     {
         private static X509KeyStorageFlags STORAGE_FLAGS = X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.Exportable;
         private IHostingEnvironment Environment;
 
-        public SignMessageController(IHostingEnvironment _environment)
+        public SignMessageController(IHostingEnvironment _environment,
+            IHttpContextAccessor httpContextAccessor,
+            IUserService userService) : base(httpContextAccessor, userService)
         {
             Environment = _environment;
         }
-
-        public ActionResult SignMessage()
+               
+        [HttpGet("/{lang:lang}/SignMessage/SignRequest")]
+        public ActionResult SignRequest()
         {
-            string request = HttpContext.Request.Query["request"].ToString();
-
-            var KEY = this.Environment.WebRootPath + "/private/private-key.pfx";
-            string wwwPath = this.Environment.WebRootPath;
-            string contentPath = this.Environment.ContentRootPath;
-
-            var PASS = "";
-
             try
             {
-                var cert = new X509Certificate2(KEY, PASS, STORAGE_FLAGS);
+                string request = HttpContext.Request.Query["request"].ToString();
 
-                RSA csp = (RSA)cert.PrivateKey;
+                var KEY = this.Environment.WebRootPath + "/private/private-key.pfx";
+                string wwwPath = this.Environment.WebRootPath;
+                string contentPath = this.Environment.ContentRootPath;
 
-                byte[] data = new ASCIIEncoding().GetBytes(request);
-                RSACryptoServiceProvider cspStrong = new RSACryptoServiceProvider(); // 2.1 and higher: Make RSACryptoServiceProvider that can handle SHA256, SHA512
-                cspStrong.ImportParameters(csp.ExportParameters(true)); // Copy to stronger RSACryptoServiceProvider
-                byte[] hash = new SHA512CryptoServiceProvider().ComputeHash(data);  // Use SHA1CryptoServiceProvider for QZ Tray 2.0 and older
-                string base64 = Convert.ToBase64String(cspStrong.SignHash(hash, CryptoConfig.MapNameToOID("SHA512"))); // Use "SHA1" for QZ Tray 2.0 and older
-                return Content(base64, "text/plain");
+                var PASS = "";
+
+                try
+                {
+                    var cert = new X509Certificate2(KEY, PASS, STORAGE_FLAGS);
+
+                    RSA csp = (RSA)cert.PrivateKey;
+
+                    byte[] data = new ASCIIEncoding().GetBytes(request);
+                    RSACryptoServiceProvider cspStrong = new RSACryptoServiceProvider(); // 2.1 and higher: Make RSACryptoServiceProvider that can handle SHA256, SHA512
+                    cspStrong.ImportParameters(csp.ExportParameters(true)); // Copy to stronger RSACryptoServiceProvider
+                    byte[] hash = new SHA512CryptoServiceProvider().ComputeHash(data);  // Use SHA1CryptoServiceProvider for QZ Tray 2.0 and older
+                    string base64 = Convert.ToBase64String(cspStrong.SignHash(hash, CryptoConfig.MapNameToOID("SHA512"))); // Use "SHA1" for QZ Tray 2.0 and older
+                    return Content(base64, "text/plain");
+                }
+                catch (Exception ex)
+                {
+                    if ((STORAGE_FLAGS & X509KeyStorageFlags.MachineKeySet) == X509KeyStorageFlags.MachineKeySet)
+                    {
+                        // IISExpress may fail with "Invalid provider type specified"; remove MachineKeySet flag, try again
+                        STORAGE_FLAGS = STORAGE_FLAGS & ~X509KeyStorageFlags.MachineKeySet;
+                        return SignRequest();
+                    }
+                    throw ex;
+                }
             }
             catch (Exception ex)
             {
-                if ((STORAGE_FLAGS & X509KeyStorageFlags.MachineKeySet) == X509KeyStorageFlags.MachineKeySet)
-                {
-                    // IISExpress may fail with "Invalid provider type specified"; remove MachineKeySet flag, try again
-                    STORAGE_FLAGS = STORAGE_FLAGS & ~X509KeyStorageFlags.MachineKeySet;
-                    return SignMessage();
-                }
                 throw ex;
             }
+
         }
     }
 }
--- wwwroot/js/print-auth.js
+++ wwwroot/js/print-auth.js
@@ -3,7 +3,21 @@
     qz.security.setSignatureAlgorithm("SHA512"); // Since 2.1
     qz.security.setSignaturePromise(function (toSign) {
         return function (resolve, reject) {
-            fetch("/fr/SignMessage/SignMessage?request=" + toSign, { cache: 'no-store', headers: { 'Content-Type': 'text/plain' } })
+            //$.ajax({
+            //    url: '/fr/SignMessage/SignRequest?request=' + toSign,
+            //    type: "GET",
+            //    //dataType: "html",
+            //    async: false,
+            //    success: function (response) {
+            //        alert(response)
+
+            //    },
+            //    error: function (xhr, status, error) {
+            //        alert('Error');
+            //    }
+            //});
+
+            fetch("/fr/SignMessage/SignRequest?request=" + toSign, { cache: 'no-store', headers: { 'Content-Type': 'text/plain' } })
                 .then((data) => { data.ok ? resolve(data.text()) : reject(data.text()); });
         };
     });
